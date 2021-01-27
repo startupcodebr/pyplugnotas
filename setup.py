feec06dc574d3186c67bc34ded2239806c4f7f86
Rafael Sales <rafael.sales@startupcode.com.br>
2021-01-26 21:22:43 -0300

    test

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
-import setuptools
+from setuptools import setup, find_packages
 
-setuptools.setup(
+setup(
     name='pyplugnotas',
     version='0.1',
     author='Startup Code',
     author_email='suporte@startupcode.com.br',
     url='https://github.com/startupcodebr',
-    packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
+    packages=find_packages(exclude=['tests', 'tests.*']),
     install_requires=[
         'requests',
     ],
     zip_safe=False,
-    python_requires='>=3.8',
+    python_requires='>3',
 )
